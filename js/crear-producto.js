1834cb26926b47d9e3c87817d0cd123e11949195
Monica Rossi <monicarossi3@hotmail.com>
2024-01-17 15:48:01 -0300

    first commit

--- js/crear-producto.js
+++ js/crear-producto.js
@@ -59,7 +59,7 @@ const { createApp } = Vue
             fetch(this.url, options)
                 .then(function () {
                     alert("Registro grabado")
-                    window.location.href = "./productos.html";  // recarga productos.html
+                    window.location.href = "./index.html";  // recarga productos.html
                 })
                 .catch(err => {
                     console.error(err);
